325dc0ca31bd52945d5fe6ab8b2adc9dbb71b345
Edward E Greenwood <edwardgreenwood38@gmail.com>
2023-11-15 17:48:00 -0500

    step 5 completed

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
-function newImage(charPath, posLeft, posBottom) {
+function newImage(charPath, posLeft, posBottom, eventFlag) {
     let character = document.createElement('img');
     character.src = 'assets/' + charPath;
     character.style.position = 'fixed';
     character.style.left = posLeft;
     character.style.bottom = posBottom;
     document.body.append(character);
+
+    if (eventFlag === 1) {
+        character.addEventListener('click', function() {
+            character.remove();
+        })
+    }
 }
 
-newImage("green-character.gif", "100px", "100px");
-newImage("pine-tree.png", "450px", "200px");
-newImage("tree.png", "200px", "300px");
-newImage("pillar.png", "350px", "100px");
-newImage("crate.png", "150px", "200px");
-newImage("well.png", "500px", "425px");
 
+newImage("green-character.gif", "100px", "100px", 0);
+newImage("pine-tree.png", "450px", "200px", 0);
+newImage("tree.png", "200px", "300px", 0);
+newImage("pillar.png", "350px", "100px", 0);
+newImage("crate.png", "150px", "200px", 0);
+newImage("well.png", "500px", "425px", 0);
+
+newImage("sword.png", "500px", "405px", 1);
 
 
