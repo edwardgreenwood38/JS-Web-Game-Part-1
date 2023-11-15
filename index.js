d54a794af97c45a150cac417fbc2645fa9eed826
Edward E Greenwood <edwardgreenwood38@gmail.com>
2023-11-15 17:29:14 -0500

    step 3 completed

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(charPath, posLeft, posBottom) {
+    let character = document.createElement('img');
+    character.src = 'assets/' + charPath;
+    character.style.position = 'fixed';
+    character.style.left = posLeft;
+    character.style.bottom = posBottom;
+    document.body.append(character);
+}
+
+newImage("green-character.gif", "100px", "100px");
+newImage("pine-tree.png", "450px", "200px");
 
 
-let pineTree = document.createElement('img')
-pineTree.src = 'assets/pine-tree.png'
-pineTree.style.position = 'fixed'
-pineTree.style.left = '450px'
-pineTree.style.bottom = '200px'
-document.body.append(pineTree)
