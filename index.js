2a6dfe39bfc7b9af6599418c032bc396dcc989dd
Edward E Greenwood <edwardgreenwood38@gmail.com>
2023-11-15 18:12:46 -0500

    bonus started

--- index.js
+++ index.js
@@ -2,26 +2,38 @@ function newImage(charPath, posLeft, posBottom, eventFlag) {
     let character = document.createElement('img');
     character.src = 'assets/' + charPath;
     character.style.position = 'fixed';
-    character.style.left = posLeft;
-    character.style.bottom = posBottom;
+    character.style.left = posLeft + 'px';
+    character.style.bottom = posBottom + 'px';
     document.body.append(character);
 
     if (eventFlag === 1) {
-        character.addEventListener('click', function() {
+        character.addEventListener('dblclick', function() {
             character.remove();
         })
     }
 }
 
 
-newImage("green-character.gif", "100px", "100px", 0);
-newImage("pine-tree.png", "450px", "200px", 0);
-newImage("tree.png", "200px", "300px", 0);
-newImage("pillar.png", "350px", "100px", 0);
-newImage("crate.png", "150px", "200px", 0);
-newImage("well.png", "500px", "425px", 0);
+newImage("green-character.gif", 100, 100, 0);
+newImage("pine-tree.png", 450, 200, 0);
+newImage("tree.png", 200, 300, 0);
+newImage("pillar.png", 350, 100, 0);
+newImage("crate.png", 150, 200, 0);
+newImage("well.png", 500, 425, 0);
 
-newImage("sword.png", "500px", "405px", 1);
-newImage("shield.png", "165px", "185px", 1);
-newImage("staff.png", "600px", "100px", 1);
+newImage("sword.png", 500, 405, 1);
+newImage("shield.png", 165, 185, 1);
+newImage("staff.png", 600, 100, 1);
 
+
+const windowWidth = window.innerWidth;
+let windowHeight = window.innerHeight;
+
+console.log(windowWidth);
+
+for (let i = 1; i <= windowWidth; i += 100) {
+    windowHeight -= 100;
+    console.log('height: ' + windowHeight);
+    console.log('i: ' + i);
+    newImage("sky.png", i, windowHeight, 0);
+}
