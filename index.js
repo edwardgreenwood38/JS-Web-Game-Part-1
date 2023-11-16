ac94cebb9f1041525673e53d1d68bdd112440996
Edward E Greenwood <edwardgreenwood38@gmail.com>
2023-11-16 17:57:40 -0500

    bonus completed

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-function newImage(charPath, posLeft, posBottom, eventFlag) {
+function newImage(charPath, posLeft, posBottom, eventFlag, zindex) {
     let character = document.createElement('img');
     character.src = 'assets/' + charPath;
     character.style.position = 'fixed';
     character.style.left = posLeft + 'px';
     character.style.bottom = posBottom + 'px';
+    character.style.zIndex = zindex;
     document.body.append(character);
 
     if (eventFlag === 1) {
@@ -14,16 +15,16 @@ function newImage(charPath, posLeft, posBottom, eventFlag) {
 }
 
 
-newImage("green-character.gif", 100, 100, 0);
-newImage("pine-tree.png", 450, 200, 0);
-newImage("tree.png", 200, 300, 0);
-newImage("pillar.png", 350, 100, 0);
-newImage("crate.png", 150, 200, 0);
-newImage("well.png", 500, 425, 0);
+newImage("green-character.gif", 100, 100, 0, 1);
+newImage("pine-tree.png", 450, 200, 0, 1);
+newImage("tree.png", 200, 300, 0, 1);
+newImage("pillar.png", 350, 100, 0, 1);
+newImage("crate.png", 150, 200, 0, 1);
+newImage("well.png", 500, 425, 0, 1);
 
-newImage("sword.png", 500, 405, 1);
-newImage("shield.png", 165, 185, 1);
-newImage("staff.png", 600, 100, 1);
+newImage("sword.png", 500, 405, 1, 1);
+newImage("shield.png", 165, 185, 1, 1);
+newImage("staff.png", 600, 100, 1, 1);
 
 
 
@@ -32,11 +33,23 @@ newImage("staff.png", 600, 100, 1);
 const windowWidth = window.innerWidth;
 let windowHeight = window.innerHeight;
 
-console.log(windowWidth);
+//console.log(windowWidth);
 
-for (let i = 1; i <= windowWidth; i += 100) {
-    windowHeight -= 100;
-    console.log('height: ' + windowHeight);
-    console.log('i: ' + i);
-    newImage("sky.png", i, windowHeight, 0);
+for(let j = window.innerHeight; j > window.innerHeight - 500; j -= 100) {
+    for (let i = 1; i <= windowWidth; i += 100) {
+        //windowHeight = window.innerHeight - 100;
+        // console.log('height: ' + windowHeight);
+        //console.log('i: ' + i);
+        newImage("sky.png", i, j, 0, -10);
+    }
+}
+
+
+for(let j = 1; j < window.innerHeight - 400; j += 100) {
+    for (let i = 1; i <= windowWidth; i += 100) {
+        //windowHeight = window.innerHeight - 100;
+        // console.log('height: ' + windowHeight);
+        //console.log('i: ' + i);
+        newImage("grass.png", i, j, 0, -10);
+    }
 }
